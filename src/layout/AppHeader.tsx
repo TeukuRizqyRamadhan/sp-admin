4a9ff0f248cd389284b91789a5dee31408e89389
Teuku Rizqy Ramadhan <kikidotid@gmail.com>
2025-02-22 18:30:10 +0700

    admin name role

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -40,6 +40,32 @@ const AppHeader: React.FC = () => {
     };
   }, []);
 
+  const [adminName, setAdminName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAdmin = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      try {
+        const response = await fetch("http://192.168.1.224:3000/admin/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) throw new Error("Gagal mengambil data admin");
+
+        const data = await response.json();
+        setAdminName(data.name); // Sesuaikan dengan response API
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchAdmin();
+  }, []);
+
   const logout = () => {
     Swal.fire({
       title: "Yakin ingin logout?",
@@ -66,6 +92,7 @@ const AppHeader: React.FC = () => {
       }
     });
   };
+
 
   return (
     <header className=" top-0 flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
@@ -145,6 +172,7 @@ const AppHeader: React.FC = () => {
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
+            <p className="text-gray-600 dark:text-gray-400 dark:text-white">{adminName ? `Halo, ${adminName}` : "Halo, Admin"}</p>
           </div>
           {/* <!-- User Area --> */}
           <button
